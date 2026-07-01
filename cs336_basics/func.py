56f6b9e3d88b15a4e974b7740a9067bc7b752f12
Aoi <aione.moe.dev@gmail.com>
2026-07-01 22:53:09 +0800

    fix: softmax

--- cs336_basics/func.py
+++ cs336_basics/func.py
@@ -4,7 +4,7 @@ import torch
 import math
 
 
-def softmax(i: int) -> torch.Tensor:
+def softmax(x: torch.Tensor, i: int) -> torch.Tensor:
     exp_x = torch.exp(x - torch.max(x, dim=i, keepdim=True).values)
     softmaxed = exp_x / torch.sum(exp_x, dim=i, keepdim=True)
     return softmaxed
